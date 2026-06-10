ba69b3d63b6d1f7e44535714cfb56be06cffaf62
Arjita Pathak <arjita.india@gmail.com>
2026-06-10 23:42:02 +0530

    Implement bls periodogram search and phase fold plots (increased precision in the depth output)

--- transit_detector.py
+++ transit_detector.py
@@ -24,7 +24,7 @@ def run_planet_search():
             lc = lk.read(f"cleaned_data/{file_name}")
             
             #Run BLS to scan for periods between 0.5 and 15 days
-            periodogram = lc.to_periodogram(method='bls', minimum_period=0.5, maximum_period=15.0)
+            periodogram = lc.to_periodogram(method='bls', minimum_period=0.7, maximum_period=15.0)
             
             
             best_period = periodogram.period_at_max_power.value
@@ -33,7 +33,7 @@ def run_planet_search():
             best_depth = periodogram.depth_at_max_power.value
             
             print(f"-> Found Period: {best_period:.4f} days")
-            print(f"-> Found Depth: {best_depth:.4f}")
+            print(f"-> Found Depth: {best_depth:.2e}")
 
             #Phase-fold the light curve around the discovered period
             folded_lc = lc.fold(period=best_period, epoch_time=best_t0)
